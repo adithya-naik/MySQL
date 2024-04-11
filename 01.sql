9588804e08554b6545b8c380d396f4658314810e
adithya-naik <jatothadithyanaik@gmail.com>
2024-04-11 19:02:09 +0530

    renamed

--- 01.sql
+++ 01.sql
@@ -4,6 +4,7 @@ use classicmodels;
 -- where is used for condition
 -- order by for sorting
 
+
 select firstName,lastName from employees where officeCode=1 order by firstName;
 
 select firstName,email from employees where officeCode=1 order by firstName;
